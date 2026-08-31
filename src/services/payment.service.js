bf0ab05c1ae094a4ddd7ad167cdd62ab78d146b2
Mohammed-Magdy-19 <mm0036101@gmail.com>
2026-08-31 13:31:09 +0300

    I have updated payment.service.js to use ui_mode: "elements"

--- src/services/payment.service.js
+++ src/services/payment.service.js
@@ -56,7 +56,7 @@ export const createPaymentIntent = async ({
     const session = await stripe.checkout.sessions.create({
         payment_method_types: ["card"],
         mode: "payment",
-        ui_mode: "custom",
+        ui_mode: "elements",
         return_url: `${env.clientUrl || customClientUrl || "https://social-media-marketplace-five.vercel.app"}/checkout/success?session_id={CHECKOUT_SESSION_ID}`,
         client_reference_id: String(buyerId),
         customer_email: buyer?.email || undefined,
